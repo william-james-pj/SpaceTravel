1af2969533a64354c597949d0b533d7cfca5a76b
wspr <wspr@gft.com>
2022-05-09 10:52:21 -0300

    Initial Commit

---
+++ SpaceTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceTravel
+//
+//  Created by Pinto Junior, William James on 09/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
